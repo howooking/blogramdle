4cf0d154c254fd1b89ca97ebb1253928814770d5
howooking <junsgk@gmail.com>
2023-03-10 16:16:04 +0900

    react quilll error fixed

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,26 +1,37 @@
 "use client";
 
 import Container from "../components/Container";
-import { useState } from "react";
-import ReactQuill from "react-quill";
+import { useEffect, useState } from "react";
 import "react-quill/dist/quill.snow.css";
 
 export default function Write() {
   const [contentsInput, setContentsInput] = useState("");
+  // const [mounted, setMounted] = useState(false);
 
+  // useEffect(() => {
+  //   setMounted(true);
+  // }, []);
+
+  // if (mounted) {
+  const ReactQuill =
+    typeof window === "object" ? require("react-quill") : () => false;
   return (
     <Container>
       <div className='flex h-[500px] gap-5'>
         <div className='flex-[5]'>
-          {/* <ReactQuill
+          <ReactQuill
             theme='snow'
             value={contentsInput}
             onChange={setContentsInput}
             className='text-editor-height'
-          /> */}
+          />
         </div>
         <div className='flex-[2] bg-slate-200'>전송, 이미지업로드</div>
       </div>
     </Container>
   );
 }
+//   else {
+//     <>Loading</>;
+//   }
+// }
